db7569b6efaf7c5def05637b5b37ca40d8f83a43
KekJoy <91479557+KekJoy@users.noreply.github.com>
2023-03-15 17:30:59 +0500

    feature/D-04: Инициализация

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ from telebot.custom_filters import StateFilter
 from utils.set_bot_commands import set_default_commands
 
 
-@bot.message_handler(commands=['start'])
-def say_hy(message):
-    bot.send_message(message.from_user.id, "Привет! Я твой бот-помощник!")
-
-
 if __name__ == '__main__':
     bot.add_custom_filter(StateFilter(bot))
     set_default_commands(bot)
