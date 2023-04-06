903880bc3bd1a236b61aea20c2c03045f939c406
Misha29RUS <114404329+Misha29RUS@users.noreply.github.com>
2023-04-06 22:13:17 +0500

    feature/D-14:  Авторизация роли "Пользователь" (#10)
    * feature/D-14: Авторизация роли "Пользователь"
    
    * feature/D-14: Авторизация роли "Пользователь"

--- database/connection_db.py
+++ database/connection_db.py
@@ -1,6 +1,7 @@
 import sqlite3
 
-conn = sqlite3.connect('database.db', check_same_thread=False)
+
+conn = sqlite3.connect('database/database.db', check_same_thread=False)
 cursor = conn.cursor()
 
 
@@ -8,3 +9,23 @@ def db_table_val(competencies_id: int, competencies_name: str, competencies_text
     cursor.execute('INSERT INTO competencies (competencies_id, competencies_name, competencies_text) VALUES (?, ?, ?)',
                    (competencies_id, competencies_name, competencies_text))
     conn.commit()
+
+
+def auth_validation(current_id):
+    exists = cursor.execute("SELECT id FROM login_id WHERE id = ?", [current_id]).fetchone()
+    return True if exists else False
+
+
+def user_register(current_id: int, user_name: str):
+    cursor.execute("""CREATE TABLE IF NOT EXISTS login_id(
+                        id INTEGER, 
+                        login TEXT
+                    )""")
+    conn.commit()
+    cursor.execute(f"SELECT id FROM login_id WHERE id = {current_id}")
+    data = cursor.fetchone()
+    if data is None:
+        user_id = current_id
+        user_name = user_name
+        cursor.execute("INSERT INTO login_id VALUES(?,?)", (user_id, user_name))
+        conn.commit()
--- handlers/custom_handlers/user_connection.py
+++ handlers/custom_handlers/user_connection.py
@@ -1,9 +1,31 @@
+from aiogram.fsm.context import FSMContext
 from loader import rt
 from aiogram import types
 
 from keyboards.reply.user_connection import user_connection
 
+from database.connection_db import user_register, auth_validation
 
-async def user_start(message: types.Message):
-    await message.answer(text=f'Ввод кода сессии',
-                         reply_markup=user_connection)
+from states.user_info import User
+
+
+async def user_start(message: types.Message, state: FSMContext):
+    current_id = message.from_user.id
+    authorized = auth_validation(current_id)
+    if authorized:
+        await message.answer(text=f'Ввод кода сессии',
+                             reply_markup=user_connection)
+    else:
+        await message.answer("Вижу Вас в первый раз. Ввидите своё ФИО")
+        await state.set_state(User.name)
+
+
+@rt.message(User.name)
+async def user_enter_name(message: types.Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    user_data = await state.get_data()
+    current_id = message.from_user.id
+    await message.answer(f"Я Вас запомнил, {user_data['name']}")
+    await state.clear()
+    user_register(current_id, user_data['name'])
+    await user_start(message, state)
